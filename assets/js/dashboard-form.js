a3592ca33f2d063635fe404e33245a42f3670e8c
sildys <sildys@users.noreply.github.com>
2026-02-06 12:18:42 +0100

    Receipt: remove CUT HERE dashed line

--- assets/js/dashboard-form.js
+++ assets/js/dashboard-form.js
@@ -351,7 +351,7 @@ function initTransactionForm() {
                     };
 
                     const params = new URLSearchParams();
-                    params.set('v', 'print-20260206-03');
+                    params.set('v', 'print-20260206-04');
                     if (createdId) params.set('txId', createdId);
 
                     params.set('itemsMode', mode === 'quick' ? 'single' : 'full');
